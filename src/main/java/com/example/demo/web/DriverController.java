7a0a3f6fc6be51c1a154661d8d0d2fc8f162a91e
Akeed Manuideen <akeedmanuideen@gmail.com>
2020-09-19 13:49:26 +0530

    could not connect to front end before because crossorigin was not annotated, fixed this issue on one controller

--- src/main/java/com/example/demo/web/DriverController.java
+++ src/main/java/com/example/demo/web/DriverController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/drivers")
 public class DriverController {
 
