4e3d21fbbe29250d021662a75239a16fc0c7f707
Akeed Manuideen <akeedmanuideen@gmail.com>
2020-09-24 21:03:39 +0530

    before deleting unneccesary tests

--- src/test/java/com/example/demo/web/ClientControllerTest.java
+++ src/test/java/com/example/demo/web/ClientControllerTest.java
@@ -6,36 +6,50 @@
 package com.example.demo.web;
 
 import com.example.demo.entities.Customer;
+import com.example.demo.services.ClientService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  *
  * @author Akeed Manuideen
  */
 public class ClientControllerTest {
-    
+
+    @InjectMocks
+    ClientController clientController;
+
+    @Mock
+    ClientService customerServices;
+
     public ClientControllerTest() {
     }
-    
+
     @BeforeAll
     public static void setUpClass() {
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
@@ -43,6 +57,47 @@ public class ClientControllerTest {
     /**
      * Test of createNewClient method, of class ClientController.
      */
+    
+//    @Test
+//    public void testAddEmployee() 
+//    {
+//        MockHttpServletRequest request = new MockHttpServletRequest();
+//        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+//         
+//        when(customerServices.saveOrUpdateClient(any(Customer.class))).thenReturn(true);
+//         
+//        Employee employee = new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com");
+//        ResponseEntity<Object> responseEntity = employeeController.addEmployee(employee);
+//         
+//        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
+//        assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/1");
+//    }
+    
+//    @Test
+//    public void testFindAll() 
+//    {
+//        // given
+//        Customer customer1 = new Customer();
+//        Customer customer2 = new Customer();
+//        Customers employees = new Customers();
+//        employees.setEmployeeList(Arrays.asList(employee1, employee2));
+// 
+//        when(customerServices.getAllCustomers()).thenReturn(employees);
+// 
+//        // when
+//        Employees result = employeeController.getEmployees();
+// 
+//        // then
+//        assertThat(result.getEmployeeList().size()).isEqualTo(2);
+//         
+//        assertThat(result.getEmployeeList().get(0).getFirstName())
+//                        .isEqualTo(employee1.getFirstName());
+//         
+//        assertThat(result.getEmployeeList().get(1).getFirstName())
+//                        .isEqualTo(employee2.getFirstName());
+//    }
+    
+    
     @Test
     public void testCreateNewClient() {
         System.out.println("createNewClient");
@@ -115,5 +170,5 @@ public class ClientControllerTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
 }
